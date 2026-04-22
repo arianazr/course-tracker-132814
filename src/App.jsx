e6ee76cc7bfa04d9bc1080140dd4d0e4714a2191
arianazr <arian.aziri50@gmail.com>
2026-04-22 15:14:42 +0200

    Added option to view each course

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import StudentCard from "./StudentCard";
 
 function App() {
 
     const courses = [
         { id: 1, name: "Client Side Programing", credits: 6, grade: 100, attending: true, difficulty: "Moderate" },
-        { id: 2, name: "Linear Algebra", credits: 6, grade: 70, attending: false, difficulty: "Hard" },
+        { id: 2, name: "Linear Algebra", credits: 6, grade: 40, attending: false, difficulty: "Hard" },
         { id: 3, name: "Calculus", credits: 6, grade: 80, attending: false, difficulty: "Hard" },
         { id: 4, name: "English For CST", credits: 3, grade: 60, attending: true, difficulty: "Easy" },
     ];
+
+    let [selectedCourse, setSelectedCourse] = useState(courses[0]);
 
     return <>
         <h1>Arian Aziri, ID: 132814</h1>
@@ -21,11 +24,12 @@ function App() {
                     <p>Grade: {course.grade}</p>
                     <p>{course.attending ? "You are attending" : "You are not attending"}</p>
                     <p>Diffculty: {course.difficulty}</p>
+                    <button onClick={() => setSelectedCourse(course)}>Show details for this course</button>
                     <hr />
                 </div >
             })
         }
-        <StudentCard course={courses[0]} />
+        <StudentCard course={selectedCourse} />
     </>
 
 }
